c695f6a590786ef02d85705921a2ea4d5d50ff8b
sheeba-merlyn1 <shebamerlin8@gmail.com>
2025-06-02 21:19:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-echo "Welcome to My Project!";
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: '.$uri.'/dashboard/');
+	exit;
 ?>
